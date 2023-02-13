44bd9989fcc54ba105c5aac1165e856beaf1d757
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-02-13 03:05:43 +0100

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel
+    """
+    name = ""
