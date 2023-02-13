790757f831b62d5955f4c9600f8a2998949500d4
Uchenna Iwuchukwu Oreoluwa <64827412+Uchennaore@users.noreply.github.com>
2023-02-13 03:09:54 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module inherits from BaseModel class
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    public class attrb name
+    """
+    name = ""
