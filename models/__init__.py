5629860177dac1c03293fe5197b524c5b59f31d6
Optimst-Techfinger <110993218+Optimst-Techfinger@users.noreply.github.com>
2023-02-14 20:20:31 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Instantiation file that starts the file storage system
+Instantiation file that starts the file storage system.
 """
 from models.engine import file_storage
 from models.base_model import BaseModel
